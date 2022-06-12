359b8c942768f35a8a23c4f13e4a3a289214b0c8
syaiful505 <syaifulstyle505@gmail.com>
2022-06-12 21:29:07 +0700

    Debug Ambil params

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,9 @@
+import { useParams} from 'react-router-dom';
 import React from 'react';
 
 const Detail = () => {
+    const params = useParams();
+    console.log(params);
     return <h1>Detail Pages</h1>
 }
 
