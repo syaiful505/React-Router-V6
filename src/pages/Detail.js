8c3c04721fd31ea1824fbc093dfb63e40d20efb1
syaiful505 <syaifulstyle505@gmail.com>
2022-06-20 21:06:01 +0700

    Params hooks

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,10 +1,38 @@
-import { useParams} from 'react-router-dom';
-import React from 'react';
+import {
+  useParams
+} from 'react-router-dom';
+import {
+  useState,
+  useEffect
+} from 'react';
 
 const Detail = () => {
-    const params = useParams();
-    console.log(params);
-    return <h1>Detail Pages</h1>
+  const {
+    id
+  } = useParams();
+  const [
+    user,
+    setUser
+  ] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch(err => console.debug(err));
+  }, [id]);
+  return <div >
+    <
+    h1 > Detail Page < /h1> <
+  h2 > Params id: {
+    id
+  } < /h2> <
+  pre > {
+    JSON.stringify(user, null, 2)
+  } < /pre> < /
+  div >
 }
 
 export default Detail;
